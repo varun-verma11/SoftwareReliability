9536a3175d9cb8b36cfb68034fbeaa19f5c4be43
Paul Colea <pc2211@ic.ac.uk>
2014-10-26 17:40:39 +0000

    [Task 1 - negative lengths] Annotated the Seat.java class.

--- Task1/src/bookings/Seat.java
+++ Task1/src/bookings/Seat.java
@@ -2,6 +2,9 @@ package bookings;
 
 public class Seat {
 
+    //@ invariant row >= 'A';
+    //@ invariant number >= 0;
+
     public static final char MIN_ROW = 'A';
     public static final char MAX_ROW = 'G';
     public static final int MIN_NUMBER = 1;
@@ -10,15 +13,19 @@ public class Seat {
     private final char row;
     private final int number;
     
+    //@ requires row >= 'A';
+    //@ requires number >= 0;
     public Seat(char row, int number) {
         this.row = row;
         this.number = number;
     }
 
+    //@ ensures \result >= 'A';
     public final char getRow() {
         return row;
     }
 
+    //@ ensures \result >= 0;
     public final int getNumber() {
         return number;
     }
