d73edc6ef4cb4dded974dbbb4f3608f40dcb2cc3
Paul Colea <pc2211@ic.ac.uk>
2014-10-26 19:31:42 +0000

    [Task 1 - index too large] annotated the Seat.java class.

--- Task1/src/bookings/Seat.java
+++ Task1/src/bookings/Seat.java
@@ -2,8 +2,8 @@ package bookings;
 
 public class Seat {
 
-    //@ invariant row >= MIN_ROW;
-    //@ invariant number >= MIN_NUMBER;
+    //@ invariant row >= MIN_ROW && row <= MAX_ROW;
+    //@ invariant number >= MIN_NUMBER && number <= MAX_NUMBER;
 
     public static final char MIN_ROW = 'A';
     public static final char MAX_ROW = 'G';
@@ -13,19 +13,19 @@ public class Seat {
     private final char row;
     private final int number;
     
-    //@ requires row >= MIN_ROW;
-    //@ requires number >= MIN_NUMBER;
+    //@ requires row >= MIN_ROW && row <= MAX_ROW;
+    //@ requires number >= MIN_NUMBER && number <= MAX_NUMBER;
     public Seat(char row, int number) {
         this.row = row;
         this.number = number;
     }
 
-    //@ ensures \result >= MIN_ROW;
+    //@ ensures \result >= MIN_ROW && \result <= MAX_ROW;
     public final char getRow() {
         return row;
     }
 
-    //@ ensures \result >= MIN_NUMBER;
+    //@ ensures \result >= MIN_NUMBER && \result <= MAX_NUMBER;
     public final int getNumber() {
         return number;
     }
--- Task1/src/run.sh
+++ Task1/src/run.sh
@@ -3,9 +3,9 @@ source /vol/lab/cs4/SoftwareReliability/coursework1.sh
 
 switch ($#)
   case 0:
-    escjava2 -VCLimit 5000000 -NoCautions bookings/*.java -NoWarn IndexTooBig -NoWarn IndexNegative -NoWarn ArrayStore -NoWarn Modifies;
+    escjava2 -VCLimit 5000000 -NoCautions -NoWarn ArrayStore -NoWarn Modifies bookings/*.java;
     breaksw
   case 1:
-    escjava2 -VCLimit 5000000 -NoCautions -NoWarn IndexTooBig -NoWarn IndexNegative -NoWarn ArrayStore -NoWarn Modifies $*;
+    escjava2 -VCLimit 5000000 -NoCautions -NoWarn ArrayStore -NoWarn Modifies $*;
     breaksw
 endsw
