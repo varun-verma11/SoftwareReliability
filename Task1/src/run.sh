f4bbd6bf542828aa3e9dd4ffccabf418f50880a5
Paul Colea <pc2211@ic.ac.uk>
2014-10-26 15:37:31 +0000

    Modify script to allow to pass in arguments to escjava

--- Task1/src/run.sh
+++ Task1/src/run.sh
@@ -1,3 +1,11 @@
+#!/bin/tcsh -f
 source /vol/lab/cs4/SoftwareReliability/coursework1.sh
 
-escjava2 -VCLimit 5000000 -NoCautions bookings/*.java -NoWarn IndexTooBig -NoWarn IndexNegative -NoWarn ArrayStore -NoWarn Modifies
+switch ($#)
+  case 0:
+    escjava2 -VCLimit 5000000 -NoCautions bookings/*.java -NoWarn IndexTooBig -NoWarn IndexNegative -NoWarn ArrayStore -NoWarn Modifies;
+    breaksw
+  case 1:
+    escjava2 -VCLimit 5000000 -NoCautions -NoWarn IndexTooBig -NoWarn IndexNegative -NoWarn ArrayStore -NoWarn Modifies $*;
+    breaksw
+endsw
