b6c987eb3e683b18d3d341a5df055252d0479634
Paul Colea <pc2211@ic.ac.uk>
2014-10-29 11:03:58 +0000

    Wrote basic version of the demo.

--- Task1/src/bookings/SeatReservationDemo.java
+++ Task1/src/bookings/SeatReservationDemo.java
@@ -3,7 +3,10 @@ package bookings;
 public class SeatReservationDemo {
 
     public static void main(String[] args) throws ReservationException {
-        
+      SeatReservationManager m = new SeatReservationManager();
+      m.reserve(new Seat('A', 5), new Customer());
+      m.reserve(new Seat('D', 14), new Customer());
+      m.reserve(new Seat('G', 11), new Customer());
     }
     
 }
