de965da5b1f229085f3175b48f6f1fc45f878f1e
Varun Verma <vv311@ic.ac.uk>
2014-11-16 15:23:35 +0000

    did the expression translation bit, got to work on property and transitions

--- Task2/SRTool/srtool/tool/src/srt/tool/ExprToSmtlibVisitor.java
+++ Task2/SRTool/srtool/tool/src/srt/tool/ExprToSmtlibVisitor.java
@@ -8,109 +8,135 @@ import srt.ast.TernaryExpr;
 import srt.ast.UnaryExpr;
 import srt.ast.visitor.impl.DefaultVisitor;
 
-public class ExprToSmtlibVisitor extends DefaultVisitor {
-	
-	public ExprToSmtlibVisitor() {
-		super(false);
-	}
+public class ExprToSmtlibVisitor extends DefaultVisitor
+{
 
-	@Override
-	public String visit(BinaryExpr expr) {
-		String operator = null;
-		switch(expr.getOperator())
-		{
-			case BinaryExpr.ADD:
-				operator = "(bvadd %s %s)";
-				break;
-			case BinaryExpr.BAND:
-				break;
-			case BinaryExpr.BOR:
-				break;
-			case BinaryExpr.BXOR:
-				break;
-			case BinaryExpr.DIVIDE:
-				break;
-			case BinaryExpr.LSHIFT:
-				break;
-			case BinaryExpr.MOD:
-				break;
-			case BinaryExpr.MULTIPLY:
-				break;
-			case BinaryExpr.RSHIFT:
-				break;
-			case BinaryExpr.SUBTRACT:
-				break;
-				
-			case BinaryExpr.LAND:
-				break;
-			case BinaryExpr.LOR:
-				break;
-			
-			case BinaryExpr.GEQ:
-				break;
-			case BinaryExpr.GT:
-				break;
-			case BinaryExpr.LEQ:
-				break;
-			case BinaryExpr.LT:
-				break;
-			case BinaryExpr.NEQUAL:
-				break;
-			case BinaryExpr.EQUAL:
-				break;
-			default:
-				throw new IllegalArgumentException("Invalid binary operator");
-		}
-		
-		return String.format(operator, visit(expr.getLhs()), visit(expr.getRhs()));
-		
-	}
+    public ExprToSmtlibVisitor()
+    {
+        super(false);
+    }
 
-	@Override
-	public String visit(DeclRef declRef) {
-		return null;
-	}
+    @Override
+    public String visit(BinaryExpr expr)
+    {
+        String operator = null;
+        switch (expr.getOperator())
+        {
+        case BinaryExpr.ADD:
+            operator = "(bvadd %s %s)";
+            break;
+        case BinaryExpr.BAND:
+            operator = "(bvand %s %s)";
+            break;
+        case BinaryExpr.BOR:
+            operator = "(bvor %s %s)";
+            break;
+        case BinaryExpr.BXOR:
+            operator = "(bvxor %s %s)";
+            break;
+        case BinaryExpr.DIVIDE:
+            operator = "(bvsdiv %s %s)";
+            break;
+        case BinaryExpr.LSHIFT:
+            operator = "(bvshl %s %s)";
+            break;
+        case BinaryExpr.MOD:
+            operator = "(bvsrem %s %s)";
+            break;
+        case BinaryExpr.MULTIPLY:
+            operator = "(bvmul %s %s)";
+            break;
+        case BinaryExpr.RSHIFT:
+            operator = "(bvashr %s %s)";
+            break;
+        case BinaryExpr.SUBTRACT:
+            operator = "(bvsub %s %s)";
+            break;
+        case BinaryExpr.LAND:
+            operator = "(tobv32 (and (tobool %s) (tobool %s)))";
+            break;
+        case BinaryExpr.LOR:
+            operator = "(tobv32 (or (tobool %s) (tobool %s)))";
+            break;
+        case BinaryExpr.GEQ:
+            operator = "(tobv32 (bvsge %s %s))";
+            break;
+        case BinaryExpr.GT:
+            operator = "(tobv32 (bvsgt %s %s))";
+            break;
+        case BinaryExpr.LEQ:
+            operator = "(tobv32 (bvsle %s %s))";
+            break;
+        case BinaryExpr.LT:
+            operator = "(tobv32 (bvslt %s %s))";
+            break;
+        case BinaryExpr.NEQUAL:
+            operator = "(tobv32 (not (= %s %s)))";
+            break;
+        case BinaryExpr.EQUAL:
+            operator = "(tobv32 (= %s %s))";
+            break;
+        default:
+            throw new IllegalArgumentException("Invalid binary operator");
+        }
 
-	@Override
-	public String visit(IntLiteral intLiteral) {
-		return null;
-	}
+        return String.format(operator, visit(expr.getLhs()), visit(expr.getRhs()));
 
-	@Override
-	public String visit(TernaryExpr ternaryExpr) {
-		return null;
-	}
+    }
 
-	@Override
-	public String visit(UnaryExpr unaryExpr) {
-		String operator = null;
-		switch(unaryExpr.getOperator())
-		{
-		case UnaryExpr.UMINUS:
-			operator = "(bvneg %s)";
-			break;
-		case UnaryExpr.UPLUS:
-			break;
-		case UnaryExpr.LNOT:
-			break;
-		case UnaryExpr.BNOT:
-			break;
-		default:
-			throw new IllegalArgumentException("Invalid binary operator");
-		}
-		
-		return String.format(operator, visit(unaryExpr.getOperand()));
-	}
-	
-	
-	/* Overridden just to make return type String. 
-	 * @see srt.ast.visitor.DefaultVisitor#visit(srt.ast.Expr)
-	 */
-	@Override
-	public String visit(Expr expr) {
-		return (String) super.visit(expr);
-	}
-	
-	
+    @Override
+    public String visit(DeclRef declRef)
+    {
+        return declRef.getName();
+    }
+
+    @Override
+    public String visit(IntLiteral intLiteral)
+    {
+        return "(_ bv" + Integer.toString(intLiteral.getValue()) + " 32)";
+    }
+
+    @Override
+    public String visit(TernaryExpr ternaryExpr)
+    {
+        return null;
+
+    }
+
+    @Override
+    public String visit(UnaryExpr unaryExpr)
+    {
+        String operator = null;
+        switch (unaryExpr.getOperator())
+        {
+        case UnaryExpr.UMINUS:
+            operator = "(bvneg %s)";
+            break;
+        case UnaryExpr.UPLUS:
+            operator = "(%s)";
+            break;
+        case UnaryExpr.LNOT:
+            operator = "(tobv32 (not (tobool %s)))";
+            break;
+        case UnaryExpr.BNOT:
+            operator = "(bvnot %s)";
+            break;
+        default:
+            throw new IllegalArgumentException("Invalid binary operator");
+        }
+
+        return String.format(operator, visit(unaryExpr.getOperand()));
+    }
+
+    /*
+     * Overridden just to make return type String.
+     * 
+     * @see srt.ast.visitor.DefaultVisitor#visit(srt.ast.Expr)
+     */
+    @Override
+    public String visit(Expr expr)
+    {
+        return (String) super.visit(expr);
+    }
 
 }
--- Task2/SRTool/srtool/tool/src/srt/tool/SMTLIBQueryBuilder.java
+++ Task2/SRTool/srtool/tool/src/srt/tool/SMTLIBQueryBuilder.java
@@ -1,31 +1,73 @@
 package srt.tool;
 
-public class SMTLIBQueryBuilder {
+import srt.ast.AssignStmt;
 
-	private ExprToSmtlibVisitor exprConverter;
-	private CollectConstraintsVisitor constraints;
-	private String queryString = "";
+public class SMTLIBQueryBuilder
+{
 
-	public SMTLIBQueryBuilder(CollectConstraintsVisitor ccv) {
-		this.constraints = ccv;
-		this.exprConverter = new ExprToSmtlibVisitor();
-	}
+    private ExprToSmtlibVisitor exprConverter;
+    private CollectConstraintsVisitor constraints;
+    private String queryString = "";
 
-	public void buildQuery() {
-		StringBuilder query = new StringBuilder();
-		query.append("(set-logic QF_BV)\n"
-				+ "(define-fun tobv32 ((p Bool)) (_ BitVec 32) (ite p (_ bv1 32) (_ bv0 32)))\n");
-		// TODO: Define more functions above (for convenience), as needed.
+    public SMTLIBQueryBuilder(CollectConstraintsVisitor ccv)
+    {
+        this.constraints = ccv;
+        this.exprConverter = new ExprToSmtlibVisitor();
+    }
 
-		// TODO: Declare variables, add constraints, add properties to check
-		// here.
+    public void buildQuery()
+    {
+        StringBuilder query = new StringBuilder();
+        query.append("(set-logic QF_BV)\n" //
+                + "(declare-sort Int 0)\n" //
+                + "(define-fun tobv32 ((p Bool)) (_ BitVec 32) (ite p (_ bv1 32) (_ bv0 32)))\n" //
+                + "(define-fun tobv32 ((p Bool)) (_ BitVec 32) (ite p (_ bv1 32) (_ bv0 32)))\n");
+        // TODO: Define more functions above (for convenience), as needed.
 
-		query.append("(check-sat)\n");
-		queryString = query.toString();
-	}
+        // TODO: Declare variables, add constraints, add properties to check
+        // here.
 
-	public String getQuery() {
-		return queryString;
-	}
+        // declare variables
+        for (String v : constraints.variableNames)
+        {
+            query.append("(declare-fun " + v + " () (BitVec 32))\n");
+        }
+
+        // add constraints
+        for (AssignStmt stmt : constraints.transitionNodes)
+        {
+            query.append("assert (bv32tobool " + exprConverter.visit(stmt) + "))\n");
+        }
+
+        // add properties
+        {
+            String props = "";
+            for (int i = 0; i < constraints.propertyNodes.size(); i++)
+            {
+                props += "(declare-fun prop" + i + " () (Bool))\n";
+            }
+
+            for (int i = 0; i < constraints.propertyNodes.size(); i++)
+            {
+                props += "(assert  (= prop" + i + " (not (bv32tobool " + exprConverter.visit(constraints.propertyNodes.get(i)) + "))))\n";
+            }
+
+            props += "(assert (or ";
+            for (int i = 0; i < constraints.propertyNodes.size(); i++)
+            {
+                props += "prop" + i + " ";
+            }
+            props = props.trim() + ("))\n"); // performance
+            query.append(props);
+        }
+
+        query.append("(check-sat)\n");
+        queryString = query.toString();
+    }
+
+    public String getQuery()
+    {
+        return queryString;
+    }
 
 }
