400603f2dd0a58090452efa50221ac6f40642355
Paul Colea <pc2211@ic.ac.uk>
2014-11-27 12:46:00 +0000

    Refactored the SMTLIBQueryBuilder, added two helper methods to append to the SB.

--- Task2/SRTool/srtool/tool/src/srt/tool/SMTLIBQueryBuilder.java
+++ Task2/SRTool/srtool/tool/src/srt/tool/SMTLIBQueryBuilder.java
@@ -8,6 +8,7 @@ public class SMTLIBQueryBuilder {
 	private ExprToSmtlibVisitor exprConverter;
 	private CollectConstraintsVisitor constraints;
 	private String queryString = "";
+	private StringBuilder query;
 
 	public static final String TOBOOL = "tobool";
 	public static final String TOBV32 = "tobv32";
@@ -15,62 +16,70 @@ public class SMTLIBQueryBuilder {
 	public SMTLIBQueryBuilder(CollectConstraintsVisitor ccv) {
 		this.constraints = ccv;
 		this.exprConverter = new ExprToSmtlibVisitor();
+		this.query = new StringBuilder();
 	}
 
 	public void buildQuery() {
-		StringBuilder query = new StringBuilder();
-		query.append("(set-logic QF_BV)\n" //
-				+ "(define-fun "
-				+ TOBOOL
-				+ " ((p (_ BitVec 32))) Bool (not (= p (_ bv0 32))))\n" //
-				+ "(define-fun "
-				+ TOBV32
-				+ " ((p Bool)) (_ BitVec 32) (ite p (_ bv1 32) (_ bv0 32)))\n");
+		appendToQuery("(set-logic QF_BV)\n");
+		appendToQuery(
+				"(define-fun %s ((p (_ BitVec 32))) Bool (not (= p (_ bv0 32))))\n",
+				TOBOOL);
+		appendToQuery(
+				"(define-fun %s ((p Bool)) (_ BitVec 32) (ite p (_ bv1 32) (_ bv0 32)))\n",
+				TOBV32);
 
 		// Declare variables.
 		for (String v : constraints.variableNames) {
-			query.append("(declare-fun " + v + " () (_ BitVec 32))\n");
+			appendToQuery("(declare-fun %s () (_ BitVec 32))\n", v);
 		}
 
 		// Add constraints for assignment checks.
 		for (AssignStmt stmt : constraints.transitionNodes) {
-			query.append("(assert (= " + stmt.getLhs().getName() + " "
-					+ exprConverter.visit(stmt.getRhs()) + "))\n");
+			appendToQuery("(assert (= %s %s ))\n", stmt.getLhs().getName(),
+					exprConverter.visit(stmt.getRhs()));
 		}
 
 		// Add checks for assertions.
 		int currentI = 0;
 		for (AssertStmt assertStmt : constraints.propertyNodes) {
-			query.append("(define-fun prop" + currentI + " () Bool (not ("
-					+ TOBOOL + " "
-					+ exprConverter.visit(assertStmt.getCondition()) + ")))\n");
+			appendToQuery("(define-fun prop%s () Bool (not (%s %s)))\n",
+					String.valueOf(currentI), TOBOOL,
+					exprConverter.visit(assertStmt.getCondition()));
 			currentI++;
 		}
 
 		// Add checks for all the properties.
-		query.append("(assert ");
+		appendToQuery("(assert ");
 		for (int i = 0; i < constraints.propertyNodes.size(); i++) {
-			query.append("(or prop" + i + "");
+			appendToQuery("(or prop%s", String.valueOf(i));
 		}
 		for (int i = 0; i <= constraints.propertyNodes.size(); i++) {
-			query.append(")");
+			appendToQuery(")");
 		}
-		query.append("\n");
+		appendToQuery("\n");
 
-		query.append("(check-sat)\n");
-		buildGetValueProperties(query);
+		appendToQuery("(check-sat)\n");
+		buildGetValueProperties();
 		queryString = query.toString();
 	}
 
-	private void buildGetValueProperties(StringBuilder query) {
-		query.append("(get-value (");
+	private void buildGetValueProperties() {
+		appendToQuery("(get-value (");
 		for (int i = 0; i < constraints.propertyNodes.size(); i++) {
-			query.append("prop" + i);
+			appendToQuery("prop%s", String.valueOf(i));
 			if (i < constraints.propertyNodes.size() - 1) {
-				query.append(" ");
+				appendToQuery(" ");
 			}
 		}
-		query.append("))\n\n");
+		appendToQuery("))\n\n");
+	}
+
+	private void appendToQuery(String s) {
+		query.append(s);
+	}
+
+	private void appendToQuery(String s, Object... args) {
+		query.append(String.format(s, args));
 	}
 
 	public String getQuery() {
