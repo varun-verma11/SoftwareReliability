58b05fd39da6fb5cadc0142f9a3c114f24feb6d5
Paul Colea <pc2211@ic.ac.uk>
2014-11-29 17:05:50 +0000

    Now running the good tests lol.

--- Task2/SRTool/srtool/run_tests.sh
+++ Task2/SRTool/srtool/run_tests.sh
@@ -16,25 +16,25 @@ TOOL=$TOOL_DIR/srt_run.sh
 
 # Loop-free tests (should work with all modes)
 
-$TESTER $TOOL tests/loopfree $ONLY_REPORT_FAILURES -mode bmc
-$TESTER $TOOL tests/loopfree $ONLY_REPORT_FAILURES -mode bmc -unsound
-$TESTER $TOOL tests/loopfree $ONLY_REPORT_FAILURES -mode verifier
-#$TESTER $TOOL tests/loopfree $ONLY_REPORT_FAILURES -mode houdini
+#$TESTER $TOOL tests/loopfree $ONLY_REPORT_FAILURES -mode bmc
+#$TESTER $TOOL tests/loopfree $ONLY_REPORT_FAILURES -mode bmc -unsound
+#$TESTER $TOOL tests/loopfree $ONLY_REPORT_FAILURES -mode verifier
+TESTER $TOOL tests/loopfree $ONLY_REPORT_FAILURES -mode houdini
 #$TESTER $TOOL tests/loopfree $ONLY_REPORT_FAILURES -mode invgen
 # Uncomment if you implement comp
 #$TESTER $TOOL tests/loopfree $ONLY_REPORT_FAILURES -mode comp
 
 # BMC sound tests
-$TESTER $TOOL tests/bmc_sound $ONLY_REPORT_FAILURES -mode bmc
+#$TESTER $TOOL tests/bmc_sound $ONLY_REPORT_FAILURES -mode bmc
 
 # BMC unsound tests
-$TESTER $TOOL tests/bmc_unsound $ONLY_REPORT_FAILURES -mode bmc -unsound
+#$TESTER $TOOL tests/bmc_unsound $ONLY_REPORT_FAILURES -mode bmc -unsound
 
 # Verifier tests
-$TESTER $TOOL tests/verifier $ONLY_REPORT_FAILURES -mode verifier
+#$TESTER $TOOL tests/verifier $ONLY_REPORT_FAILURES -mode verifier
 
 # Houdini tests
-#$TESTER $TOOL tests/houdini $ONLY_REPORT_FAILURES -mode houdini
+$TESTER $TOOL tests/houdini $ONLY_REPORT_FAILURES -mode houdini
 
 # Invariant generation tests
 #$TESTER $TOOL tests/invgen $ONLY_REPORT_FAILURES -mode invgen
