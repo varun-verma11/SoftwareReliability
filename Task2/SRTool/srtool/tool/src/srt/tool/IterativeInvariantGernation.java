9ae4f4db27602f675c124a4c478abf95e68f8942
Varun Verma <vv311@ic.ac.uk>
2014-11-30 21:03:21 +0000

    dummy class for iterative invgen

---
+++ Task2/SRTool/srtool/tool/src/srt/tool/IterativeInvariantGernation.java
@@ -0,0 +1,11 @@
+package srt.tool;
+
+import srt.ast.visitor.impl.DefaultVisitor;
+
+public class IterativeInvariantGernation extends DefaultVisitor {
+
+	public IterativeInvariantGernation() {
+		super(true);
+	}
+
+}
